009084a77ef0ac9d14cbaac881a6083b07c564ad
Christopher Giroir <kelsin@valefor.com>
2009-04-17 04:48:03 +0000

    Initial commit with simple starting template

---
+++ default.rb
@@ -0,0 +1,39 @@
+# Config Setup
+initializer 'config.rb', <<-'CODE'
+CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml") || {}
+CONFIG = (CONFIG['default'] || {}).symbolize_keys.merge((CONFIG[RAILS_ENV] || {}).symbolize_keys)
+CODE
+
+file 'config/config.yml', <<-'CODE'
+default:
+
+development:
+
+production:
+
+CODE
+
+# Clear the default index
+run "rm public/index.html"
+
+# Git Setup
+file '.gitignore', <<-'CODE'
+log/*.log
+tmp/*
+doc/app
+doc/api
+.DS_Store
+db/*.db
+db/*.sqlite3
+CODE
+
+# Git empty directories
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/example_database.yml"
+
+# Repo commands
+git :init
+git :add => "."
+git :commit => "-a -m 'Initial commit'"
+
+
