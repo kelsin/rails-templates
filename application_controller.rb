fa77af9c3e2fd4c48c6b7ed61bcb380b1e79b81b
Christopher Giroir <kelsin@valefor.com>
2009-12-04 00:23:50 -0500

    Slightly different than default

--- application_controller.rb
+++ application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
   # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
+  filter_parameter_logging :password, :password_confirmation
   
-  helper_method :current_user
+  helper_method :current_user, :current_user_session
   
   private
   
